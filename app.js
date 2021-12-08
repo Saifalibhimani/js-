8e57330a57d0c8c6265fbf6349dab29f36cb0d8a
Saifalibhimani <94796479+Saifalibhimani@users.noreply.github.com>
2021-12-08 23:20:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,86 @@
+//  alert ("Welcome")
+//  alert ("Error! Please enter a valid password")
+//  alert ("Welcome to JS Land... \ Happy Coding!")
+//  alert ("Welcome to Js land...")
+//  var x = (50);
+//  console.log (x + 20);
+
+//  var age = 30;
+//  alert(age);
+//  alert("you have visited this site 5 times")
+//  var birthyear = "1999"
+//  document.write("my birth year is" + birthyear)
+//  var visitor = "john doe"
+//  var product = "T-shirt"
+//  var quantity = "5"
+//  document.write(visitor + "ordered" + quantity + product + "on XYZ Clothing story") 
+
+
+
+
+
+
+
+
+// // var a=prompt()
+// // var b= 18
+
+// // if(a > b) {
+// //     console.log("User Allowed");
+// // } else {
+// //   if(c) {
+// //       console.log("UserAllowed on Card")
+// //  } else{  
+// //     console.log("User Not Allowed")
+// // }
+// // }
+
+// // var age = prompt
+// // var age = true;
+// // var t= "saims"
+
+// // if (age > 18 || c == true) {
+// //     console.log("User Allowed");
+// // }
+
+// // if(c== true && t == "saims"){
+// //     console.log ("User Allowed");
+// // } 
+// // && and operator
+// // || or operator
+
+
+// var a = "apple";
+// var b = "orange";
+// var c = "pineapple";
+// var d = "mango";
+// var e = "shareefa";
+
+// var fruits = ["apple","mango","orange"];
+  
+// fruits[2] = "asdasda"
+
+// console.log(fruits);
+
+//  var arr =["abc", 123,true,prompt()]
+
+//  console.log(arr);
+// arr.push("abc");
+// arr.push(123);
+// arr.push(true);
+// arr.pop();
+// arr.unshift("xyz");
+// arr.shift();
+// var newArrayDeleted =arr.splice(3,4);
+// var newArray 
+
+// var arr =["a","b","c","d"];
+
+// var word ='c'
+// var input =prompt();
+
+// for(var i =0; i < arr.length; i++) {
+//     if (word == arr[i]) {
+   
+//     }
+// }
